6903fc98fa624311e93c33e0451634c2ad64f987
Marcio Victor <marcio9769@gmail.com>
2024-03-08 15:49:11 -0300

    fix: pequenas correções para dispositivos moveis

--- src/components/Nav/style.js
+++ src/components/Nav/style.js
@@ -5,11 +5,14 @@ export const style = {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'space-between',
-        marginBottom: '2em'
+        marginBottom: '2em',
     },
     imgLogo: {
         height: '3em',
-        paddingLeft: '3em'
+        paddingLeft: '3em',
+        '@media (max-width: 600px)': {
+            paddingLeft: '1em'
+        }
     },
     search: {
         marginRight: '1.5em',
@@ -21,6 +24,9 @@ export const style = {
         paddingLeft: '0.5em',
         '&:hover': {
             backgroundColor: 'rgba(255, 255, 255, 0.1)'
+        },
+        '@media (max-width: 600px)': {
+            marginRight: '0em'
         }
     },
     boxSnS:{
